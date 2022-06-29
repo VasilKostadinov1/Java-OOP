c2fa4a8d77a64018499b276463913ff990721680
VasilKostadinov1 <90925326+VasilKostadinov1@users.noreply.github.com>
2022-06-29 16:15:33 +0300

    Add files via upload

---
+++ Encapsulation/SortByNameAge/Person.java
@@ -0,0 +1,33 @@
+package Encapsulation_Lab.SortByNameAge;
+
+public class Person {
+
+    private String firstName;
+    private String lastName;
+    private int age;
+
+    public Person(String firstName, String lastName, int age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+
+    public String getFirstName() {
+        return this.firstName;
+    }
+
+    public String getLastName() {
+        return this.lastName;
+    }
+
+    public int getAge() {
+        return this.age;
+    }
+
+    public String toString() {
+        return String.format("%s %s is %d years old.",
+                this.getFirstName(),
+                this.getLastName(),
+                this.getAge());
+    }
+}
