30578efffdaaf9d6001380e6697b36798e840c44
VasilKostadinov1 <90925326+VasilKostadinov1@users.noreply.github.com>
2022-06-29 16:14:50 +0300

    Add files via upload

---
+++ Encapsulation/SalaryIncrease/Person.java
@@ -0,0 +1,56 @@
+package Encapsulation_Lab.SalaryIncrease;
+
+public class Person {
+
+    private String firstName;
+    private String lastName;
+    private int age;
+    private double salary;
+
+    public Person(String firstName, String lastName, int age, double salary) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.salary = salary;
+    }
+
+    public String getFirstName() {
+        return this.firstName;
+    }
+
+    public String getLastName() {
+        return this.lastName;
+    }
+
+    public int getAge() {
+        return this.age;
+    }
+
+    public double getSalary() {
+        return this.salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public void increaseSalary(double percentage) {
+        if (this.getAge() < 30) {
+            this.setSalary(this.getSalary() + (this.getSalary() * percentage / 200));
+
+        } else {
+            this.setSalary(this.getSalary() + (this.getSalary() * percentage / 100));
+        }
+    }
+
+    public String toString() {
+        return String.format("%s %s gets %s leva",
+                this.getFirstName(),
+                this.getLastName(),
+                this.getSalary());
+    }
+
+
+    public void increaseSalaryV(double bonus) {
+    }
+}
