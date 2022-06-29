17a960f2db0006bda9ec5ffc56d02e57f3530c2b
VasilKostadinov1 <90925326+VasilKostadinov1@users.noreply.github.com>
2022-06-29 16:16:20 +0300

    Add files via upload

---
+++ Encapsulation/ValidData/Main.java
@@ -0,0 +1,44 @@
+package Encapsulation_Lab.ValidData;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(reader.readLine());
+
+        List<Person> people = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            String[] input = reader.readLine().split("\\s+");
+
+            try {
+                String firstName = input[0];
+                String lastName = input[1];
+                int age = Integer.parseInt(input[2]);
+                double salary = Double.parseDouble(input[3]);
+
+                Person person = new Person(firstName, lastName, age, salary);
+                people.add(person);
+
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+
+        double bonus = Double.parseDouble(reader.readLine());
+
+        for (Person person : people) {
+            person.increaseSalary(bonus);
+
+            System.out.println(person.toString());
+        }
+    }
+}
