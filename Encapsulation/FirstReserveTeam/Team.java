92438f7340528a0f30a68d653da80988b053995e
VasilKostadinov1 <90925326+VasilKostadinov1@users.noreply.github.com>
2022-06-29 16:17:18 +0300

    Add files via upload

---
+++ Encapsulation/FirstReserveTeam/Team.java
@@ -0,0 +1,41 @@
+package Encapsulation_Lab.FirstAndReserveTeam;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Team {
+
+    private String name;
+    private List<Person> firstTeam;
+    private List<Person> reserveTeam;
+
+    public Team(String name) {
+        this.name = name;
+        this.firstTeam = new ArrayList<>();
+        this.reserveTeam = new ArrayList<>();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    public List<Person>getFirstTeam(){
+        return Collections.unmodifiableList(this.firstTeam);
+    }
+    public List<Person> getReserveTeam(){
+        return Collections.unmodifiableList(this.reserveTeam);
+    }
+
+    //All person younger than 40 go in first team, others go in reverse team
+    public void addPlayer(Person person) {
+        if (person.getAge() < 40) {
+            this.firstTeam.add(person);
+        } else {
+            this.reserveTeam.add(person);
+        }
+    }
+}
