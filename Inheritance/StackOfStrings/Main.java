ec3f78c715777048c4b6f5236c214e0f60a87830
VasilKostadinov1 <90925326+VasilKostadinov1@users.noreply.github.com>
2022-06-30 14:28:08 +0300

    Add files via upload

---
+++ Inheritance/StackOfStrings/Main.java
@@ -0,0 +1,22 @@
+package Inheritance_Lab.StackOfStrings_05;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        StackOfStrings sos = new StackOfStrings();
+
+        sos.push("one");
+        sos.push("two");
+        sos.push("three");
+
+        System.out.println(sos.isEmpty());
+        System.out.println(sos.peek());
+
+        System.out.println(sos.peek());
+        System.out.println(sos.peek());
+        System.out.println(sos.peek());
+    }
+}
