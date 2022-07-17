1c7e1d8ebbd1524795440794422bf0a8acc8ba26
VasilKostadinov1 <90925326+VasilKostadinov1@users.noreply.github.com>
2022-07-17 16:25:19 +0300

    Add files via upload

---
+++ Exams/6. CATs 11 Dec 2021/entities/toys/Ball.java
@@ -0,0 +1,13 @@
+package catHouse.entities.toys;
+
+public class Ball extends BaseToy{
+
+
+
+    private static final int SOFTNESS = 1;
+    private static final double PRICE = 10;
+
+    public Ball() {
+        super(SOFTNESS, PRICE);
+    }
+}
