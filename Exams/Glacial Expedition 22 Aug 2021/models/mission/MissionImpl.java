da607648a996dedaecf83b7f1771d5a171739f8b
VasilKostadinov1 <90925326+VasilKostadinov1@users.noreply.github.com>
2022-07-17 14:14:54 +0300

    Add files via upload

---
+++ Exams/Glacial Expedition 22 Aug 2021/models/mission/MissionImpl.java
@@ -0,0 +1,26 @@
+package glacialExpedition.models.mission;
+
+import glacialExpedition.models.explorers.Explorer;
+import glacialExpedition.models.states.State;
+
+import java.util.Collection;
+
+public class MissionImpl implements Mission{
+
+
+    @Override
+    public void explore(State state, Collection<Explorer> explorers) {
+        Collection<String> stateExhibits = state.getExhibits();
+
+        for (Explorer explorer : explorers) {
+
+            while (explorer.canSearch() && stateExhibits.iterator().hasNext()) {
+
+                explorer.search();
+                String currentExhibit = stateExhibits.iterator().next();
+                explorer.getSuitcase().getExhibits().add(currentExhibit);
+                stateExhibits.remove(currentExhibit);
+            }
+        }
+    }
+}
